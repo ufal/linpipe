57e1889945bc1468b46d7eb2f920c807f5aea356
Milan Straka <milan@strakovi.com>
2022-10-13 15:46:36 +0200

    Add empty implementation of Operation::reserve_models.

---
+++ src/operations/operation.cpp
@@ -0,0 +1,17 @@
+// This file is part of LinPipe <http://github.com/ufal/linpipe/>.
+//
+// Copyright 2022 Institute of Formal and Applied Linguistics, Faculty of
+// Mathematics and Physics, Charles University in Prague, Czech Republic.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "operations/operation.h"
+
+namespace linpipe {
+
+void Operation::reserve_models(PipelineState& /*state*/) {
+}
+
+} // namespace linpipe
