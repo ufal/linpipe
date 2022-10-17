36462c33f6ad225e0c3a60b616e7ac8b59f8f1ca
Jana Strakova <strakova@ufal.mff.cuni.cz>
2022-10-17 12:15:00 +0200

    Add simple Format factory method.

--- src/formats/format.cpp
+++ src/formats/format.cpp
@@ -7,24 +7,15 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#pragma once
-
-#include "common.h"
-#include "core/document.h"
+#include "format.h"
+#include "text.h"
 
 namespace linpipe {
 
-class Format {
- public:
-  virtual ~Format() {}
+unique_ptr<Format> Format::create(const string_view description) {
+  if (description == "text") return make_unique<formats::Text>();
 
-  static unique_ptr<Format> create(const string_view description);
-
-  virtual bool load(Document& document, istream& input, const string_view source_path, const string_view name="layer") = 0;
-  virtual void save(Document& document, ostream& output, const string_view name="layer") = 0;
-
- protected:
-  Format() {};
-};
+  return nullptr;
+}
 
 } // namespace linpipe
