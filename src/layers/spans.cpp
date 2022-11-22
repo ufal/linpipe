e6bcd043b63c60553a5fb1b39f8b784c085cd28b
Jana Strakova <strakova@ufal.mff.cuni.cz>
2022-11-22 12:58:46 +0100

    Add Spans layer (e.g., for named entities)

---
+++ src/layers/spans.cpp
@@ -0,0 +1,43 @@
+// This file is part of LinPipe <http://github.com/ufal/linpipe/>.
+//
+// Copyright 2022 Institute of Formal and Applied Linguistics, Faculty of
+// Mathematics and Physics, Charles University in Prague, Czech Republic.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "common.h"
+#include "layers/spans.h"
+#include "lib/json.h"
+
+namespace linpipe::layers {
+
+void Spans::from_json(const Json& json) {
+  _json_checker.json_has_string("Spans::from_json", json, "token_layer");
+  token_layer = json["token_layer"];
+
+  _json_checker.json_has_array("Spans::from_json", json, "spans");
+  spans = json["spans"].get<vector<pair<unsigned int, unsigned int>>>();
+
+  if (json.contains("tags") && json.at("tags").is_array()) {
+    tags = json["tags"].get<vector<string>>();
+  }
+}
+
+void Spans::to_json(Json& json) {
+  json["token_layer"] = token_layer;
+  json["spans"] = spans;
+  json["tags"] = tags;
+}
+
+void Spans::to_html(string& /*html*/) {
+}
+
+void Spans::decode(const vector<string>& /*encoded_tags*/, const SpanEncoding /*encoding*/) {
+}
+
+void Spans::encode(vector<string>& /*encoded_tags*/, const SpanEncoding /*encoding*/) {
+}
+
+} // namespace linpipe::layers
---
+++ src/layers/spans.h
@@ -0,0 +1,36 @@
+// This file is part of LinPipe <http://github.com/ufal/linpipe/>.
+//
+// Copyright 2022 Institute of Formal and Applied Linguistics, Faculty of
+// Mathematics and Physics, Charles University in Prague, Czech Republic.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "common.h"
+#include "layers/layer.h"
+#include "layers/tokens.h"
+
+namespace linpipe::layers {
+
+enum class SpanEncoding { bio, iob };
+
+class Spans : public Layer {
+ public:
+  Spans(const string name={}) : Layer("spans", name.empty() ? "spans" : name) {};
+
+  virtual void from_json(const Json& json) override;
+  virtual void to_json(Json& json) override;
+  virtual void to_html(string& html) override;
+
+  void decode(const vector<string>& encoded_tags, const SpanEncoding encoding = SpanEncoding::bio);
+  void encode(vector<string>& encoded_tags, const SpanEncoding encoding = SpanEncoding::bio);
+
+  string token_layer;
+  vector<pair<unsigned int, unsigned int>> spans;
+  vector<string> tags;
+};
+
+} // namespace linpipe::layers
