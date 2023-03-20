e1a82dced9a095c2dd2613d722ec37ee6a6c88fa
Dominik Farhan <nikousf@seznam.cz>
2023-03-20 17:44:51 +0100

    Add variables needed for mmaping

--- src/dev/kbelik/index.h
+++ src/dev/kbelik/index.h
@@ -7,6 +7,9 @@ using namespace kbelik {
   template<typename T>
   class Index {
     private:
+      int fd;   // TODO check for portability
+      int length; 
+      const void* mmap_addr;
       void load(string fp);
       vector<T> qids_from_wikidata(string fp);
     public:
