a3d660054f7c36feb8233a35d153e53b6c167338
Dominik Farhan <nikousf@seznam.cz>
2023-04-30 10:11:31 +0200

    Fix intendation.

--- src/dev/kbelik/map_values/int4.cpp
+++ src/dev/kbelik/map_values/int4.cpp
@@ -5,16 +5,19 @@
 #include "dev/kbelik/map_values/int4.h"
 
 namespace linpipe::kbelik::map_values {
-  size_t Int4::length(const byte* ptr) {
-    return 4;
-  }
-  void Int4::deserialize(const byte* ptr, Int4::Type& value) {
-    memcpy(&value, ptr, Int4::length(ptr));
-  }
-  void Int4::serialize(const Int4::Type& value, vector<byte>& data) {
-    size_t bytes_cnt = Int4::length(NULL);
-    data.resize(bytes_cnt);
-    memcpy(data.data(), &value, bytes_cnt);
-  }
+
+size_t Int4::length(const byte* ptr) {
+  return 4;
+}
+
+void Int4::deserialize(const byte* ptr, Int4::Type& value) {
+  memcpy(&value, ptr, Int4::length(ptr));
+}
+
+void Int4::serialize(const Int4::Type& value, vector<byte>& data) {
+  size_t bytes_cnt = Int4::length(NULL);
+  data.resize(bytes_cnt);
+  memcpy(data.data(), &value, bytes_cnt);
+}
 
 } // namespace linpipe::kbelik::map_values
