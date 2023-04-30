df3d597147aa4bfb88b0bb2fe9a9599b2402e914
Dominik Farhan <nikousf@seznam.cz>
2023-04-30 10:09:31 +0200

    Add implementation of int4.

--- src/dev/kbelik/map_values/int4.h
+++ src/dev/kbelik/map_values/int4.h
@@ -7,10 +7,10 @@ namespace linpipe::kbelik::map_values {
 
 class Int4 {
  public:
-  using Type = int4_t;
+  using Type = int32_t;
   static size_t length(const byte* ptr);
   static void deserialize(const byte* ptr, Type& value);
   static void serialize(const Type& value, vector<byte>& data);
-}
+};
 
 } // namespace linpipe::kbelik::map_values
