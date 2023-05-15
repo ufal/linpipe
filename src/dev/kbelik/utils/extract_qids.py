eecfd17a47a907d0b2db0c8b1f3e2275d61eee9a
Dominik Farhan <nikousf@seznam.cz>
2023-05-15 09:54:47 +0200

    Count number of different named entities.

--- src/dev/kbelik/utils/extract_qids.py
+++ src/dev/kbelik/utils/extract_qids.py
@@ -1,24 +1,30 @@
 """ Extracts qids from wikidata stored in WIKIDATA_DIR and save them in
 OUTPUT_FILE. Specify number of available PROCESSES. """
-from multiprocessing import Pool
+from collections import defaultdict
 import json
-import os
 from math import ceil
+from multiprocessing import Pool
+import os
 
 PROCESSES = 16
 #PROCESSES = 2
-WIKIDATA_DIR = 'wikidata'
-OUTPUT_FILE = 'qids.txt'
+WIKIDATA_DIR = '../../../../../damuel_1.0/damuel_1.0_wikidata'
+# OUTPUT_FILE = 'qids.txt'
 
 assert PROCESSES <= os.cpu_count()
 
 
 def process_file(path):
     qids = set()
+    counts = defaultdict(int)
     def process_line(line):
         try: 
             d = json.loads(line)
             qids.add(d['qid'])
+            if "named_entities" in d:
+                net = d["named_entities"]["type"]
+                for entity in net:
+                    counts[entity] += 1
         except json.JSONDecodeError:
             pass
 
@@ -26,15 +32,19 @@ def process_file(path):
         text = f.read()
     for line in text.split('\n'):
         process_line(line)
-    return qids
+    return qids, counts
 
 
 def process_files(l):
     qids = set()
+    counts = defaultdict(int)
     for path in l:
-        qids = qids.union(process_file(path))
+        file_qids, file_counts = process_file(path)
+        qids = qids.union(file_qids)
+        for k, v in file_counts.items():
+            counts[k] += v
         print("processed file", path)
-    return qids
+    return qids, counts
 
 
 def chunks(l):
@@ -50,12 +60,19 @@ paths = chunks(paths)
 print("Starting pools...")
 
 qids = set()
+counts = defaultdict(int)
 
 with Pool(PROCESSES) as p:
     res = p.map(process_files, paths)
-    for s in res:
+    for s, c in res:
         qids = qids.union(s)
+        for k, v in c.items():
+            counts[k] += v
 
-with open(OUTPUT_FILE, 'w') as f:
-    for qid in qids:
-        f.write(qid + '\n')
+total_counts = sum(counts.values())
+
+print("RESULTS:")
+print("There are total of ", len(qids), f"in the {WIKIDATA_DIR}")
+print("Named entities type frequencies")
+for k, v in counts.items():
+    print(f"type {k}, number of occurences {v}, share {round(100 * v / total_counts, 2)} %")
