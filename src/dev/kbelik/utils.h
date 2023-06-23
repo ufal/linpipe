2a852ac0ae6007d0df40f2614c4edef738bed539
Dominik Farhan <nikousf@seznam.cz>
2023-06-23 13:37:47 +0200

    Add header with the most commonly used functions.

---
+++ src/dev/kbelik/utils.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "common.h"
+
+namespace linpipe::kbelik {
+
+void split(const string& text, const string& delimiter, vector<string>& splitted) {
+  splitted = vector<string>();
+  size_t start = 0;
+  size_t end = 0;
+  while((end = text.find(delimiter, start)) != string::npos) {
+    splitted.push_back(text.substr(start, end - start));
+    start = end + delimiter.size();
+  }
+  splitted.push_back(text.substr(start));
+}
+
+} // linpipe::kbelik
