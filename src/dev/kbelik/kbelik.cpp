a356937caf316a829984e64aac0bcc6fdca789e8
Dominik Farhan <nikousf@seznam.cz>
2023-07-12 18:47:10 +0200

    Empty kbelik to stop errors

--- src/dev/kbelik/kbelik.cpp
+++ src/dev/kbelik/kbelik.cpp
@@ -7,4 +7,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include "dev/kbelik/benchmark.cpp"
+//#include "dev/kbelik/benchmark.cpp"
+
+int main(int argc, char** argv) {
+  return 0;
+}
