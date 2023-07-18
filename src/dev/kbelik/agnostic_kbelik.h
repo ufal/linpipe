16ebaecb4248bb2c5fb7e62b2bf5ece339ab846a
Dominik Farhan <nikousf@seznam.cz>
2023-07-18 21:23:25 +0200

    Remove unused includes.

--- src/dev/kbelik/agnostic_kbelik.h
+++ src/dev/kbelik/agnostic_kbelik.h
@@ -1,23 +1,11 @@
 #pragma once
 
-#include <filesystem>
-
-#include "lib/json.h"
-
 #include "common.h"
 
-#include "dev/kbelik/byte_serializer_deserializer.h"
-#include "dev/kbelik/dynamic_map.h"
-#include "dev/kbelik/id.h"
-#include "dev/kbelik/named_entity.h"
-#include "dev/kbelik/persistent_map.h"
-#include "dev/kbelik/typed_value.h"
-#include "dev/kbelik/utils.h"
+#include "dev/kbelik/general_kbelik.h"
 
 #include "dev/kbelik/map_keys/qid8.h"
 #include "dev/kbelik/map_values/agnostic_entity_info_huff.h"
-
-#include "dev/kbelik/general_kbelik.h"
 
 namespace linpipe::kbelik {
 
