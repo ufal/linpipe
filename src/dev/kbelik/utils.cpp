f9dab6a61664beabe6ec4ef711bccacb9ad15f6d
Dominik Farhan <nikousf@seznam.cz>
2023-07-22 00:15:19 +0200

    Fix bug if output and input are the same ref

--- src/dev/kbelik/utils.cpp
+++ src/dev/kbelik/utils.cpp
@@ -3,14 +3,15 @@
 namespace linpipe::kbelik {
 
 void split(const string& text, const string& delimiter, vector<string>& splitted) {
-  splitted = vector<string>();
+  vector<string> result;
   size_t start = 0;
   size_t end = 0;
   while((end = text.find(delimiter, start)) != string::npos) {
-    splitted.push_back(text.substr(start, end - start));
+    result.push_back(text.substr(start, end - start));
     start = end + delimiter.size();
   }
-  splitted.push_back(text.substr(start));
+  result.push_back(text.substr(start));
+  splitted = result;
 }
 
 } // linpipe::kbelik
