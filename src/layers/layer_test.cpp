98c423e83172508949e8c592aac85f8930ccca9a
Milan Straka <milan@strakovi.com>
2023-10-04 20:18:17 +0200

    Do not include common.h in source files.

--- src/layers/layer_test.cpp
+++ src/layers/layer_test.cpp
@@ -8,7 +8,6 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include "layers/layer.h"
-#include "lib/doctest.h"
 
 namespace linpipe {
 
--- src/layers/spans.cpp
+++ src/layers/spans.cpp
@@ -7,7 +7,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include "common.h"
 #include "layers/spans.h"
 #include "lib/json.h"
 #include "utils/json_utils.h"
