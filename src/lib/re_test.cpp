63695f793d3424229c5b1fa02b5e3f733eb0293e
Milan Straka <milan@strakovi.com>
2023-10-05 15:28:35 +0200

    Nicer comparison of vectors in the tests.
    Also, we explictly test whether .data() on match/size is nullptr,
    not just that it is empty.

--- src/lib/re_test.cpp
+++ src/lib/re_test.cpp
@@ -20,15 +20,14 @@ TEST_CASE("RE::RE") {
 TEST_CASE("RE::match") {
   SUBCASE("no groups") {
     CHECK(RE("\\w").match("Hi") == "H");
-    CHECK(RE("\\w").match("@") == string_view());
+    CHECK(RE("\\w").match("@").data() == nullptr);
     CHECK(RE("\\w").match("\xc4\x8d") == "\xc4\x8d");
     CHECK(RE("\\p{Ll}").match("\xc4\x8d") == "\xc4\x8d");
-    CHECK(RE("\\p{Ll}").match("\xc4\x8c") == string_view());
+    CHECK(RE("\\p{Ll}").match("\xc4\x8c").data() == nullptr);
   }
   SUBCASE("groups") {
     vector<string_view> groups;
-    CHECK(RE(".(\\w).(\\w)").match("abcde", &groups) == "abcd");
-    CHECK(groups.size() == 2); CHECK(groups[0] == "b"); CHECK(groups[1] == "d");
+    CHECK(RE(".(\\w).(\\w)").match("abcde", &groups) == "abcd"); CHECK(groups == vector{"b"sv, "d"sv});
   }
 }
 
@@ -41,28 +40,21 @@ TEST_CASE("RE::search") {
   SUBCASE("groups") {
     vector<string_view> groups;
     CHECK(RE("(\\w)!(\\w)").search("Hi\xc4\x8c!\xc4\x8dthere", &groups) == "\xc4\x8c!\xc4\x8d");
-    CHECK(groups[0] == "\xc4\x8c");
-    CHECK(groups[1] == "\xc4\x8d");
+    CHECK(groups == vector{"\xc4\x8c"sv, "\xc4\x8d"sv});
   }
 }
 
 TEST_CASE("RE::split") {
   vector<string_view> parts;
-  CHECK(RE(",").split("a,b,c", parts) == 3);
-  CHECK(parts.size() == 3); CHECK(parts[0] == "a"); CHECK(parts[1] == "b"); CHECK(parts[2] == "c");
+  CHECK(RE(",").split("a,b,c", parts) == 3); CHECK(parts == vector{"a"sv, "b"sv, "c"sv});
 
-  CHECK(RE(",").split("a,b,c", parts, 1) == 2);
-  CHECK(parts.size() == 2); CHECK(parts[0] == "a"); CHECK(parts[1] == "b,c");
+  CHECK(RE(",").split("a,b,c", parts, 1) == 2); CHECK(parts == vector{"a"sv, "b,c"sv});
 
-  CHECK(RE(",").split(",a,b,c,", parts) == 5);
-  CHECK(parts.size() == 5); CHECK(parts[0] == ""); CHECK(parts[1] == "a");
-  CHECK(parts[2] == "b"); CHECK(parts[3] == "c"); CHECK(parts[4] == "");
+  CHECK(RE(",").split(",a,b,c,", parts) == 5); CHECK(parts == vector{""sv, "a"sv, "b"sv, "c"sv, ""sv});
 
-  CHECK(RE("\\d+").split("a42b1c1234", parts) == 4);
-  CHECK(parts.size() == 4); CHECK(parts[0] == "a"); CHECK(parts[1] == "b"); CHECK(parts[2] == "c"); CHECK(parts[3] == "");
+  CHECK(RE("\\d+").split("a42b1c1234", parts) == 4); CHECK(parts == vector{"a"sv, "b"sv, "c"sv, ""sv});
 
-  CHECK(RE("(?<=[ab])\\d(?=b)").split("a1a2a3b4b6", parts) == 3);
-  CHECK(parts.size() == 3); CHECK(parts[0] == "a1a2a"); CHECK(parts[1] == "b"); CHECK(parts[2] == "b6");
+  CHECK(RE("(?<=[ab])\\d(?=b)").split("a1a2a3b4b6", parts) == 3); CHECK(parts == vector{"a1a2a"sv, "b"sv, "b6"sv});
 }
 
 TEST_CASE("RE::sub") {
@@ -88,15 +80,14 @@ TEST_CASE("RE32::RE32") {
 TEST_CASE("RE32::match") {
   SUBCASE("no groups") {
     CHECK(RE32("\\w").match(U"Hi") == U"H");
-    CHECK(RE32("\\w").match(U"@") == u32string_view());
+    CHECK(RE32("\\w").match(U"@").data() == nullptr);
     CHECK(RE32("\\w").match(U"\u010d") == U"\u010d");
     CHECK(RE32("\\p{Ll}").match(U"\u010d") == U"\u010d");
-    CHECK(RE32("\\p{Ll}").match(U"\u010c") == u32string_view());
+    CHECK(RE32("\\p{Ll}").match(U"\u010c").data() == nullptr);
   }
   SUBCASE("groups") {
     vector<u32string_view> groups;
-    CHECK(RE32(".(\\w).(\\w)").match(U"abcde", &groups) == U"abcd");
-    CHECK(groups.size() == 2); CHECK(groups[0] == U"b"); CHECK(groups[1] == U"d");
+    CHECK(RE32(".(\\w).(\\w)").match(U"abcde", &groups) == U"abcd"); CHECK(groups == vector{U"b"sv, U"d"sv});
   }
 }
 
@@ -109,28 +100,21 @@ TEST_CASE("RE32::search") {
   SUBCASE("groups") {
     vector<u32string_view> groups;
     CHECK(RE32("(\\w)!(\\w)").search(U"Hi\u010c!\u010dthere", &groups) == U"\u010c!\u010d");
-    CHECK(groups[0] == U"\u010c");
-    CHECK(groups[1] == U"\u010d");
+    CHECK(groups == vector{U"\u010c"sv, U"\u010d"sv});
   }
 }
 
 TEST_CASE("RE32::split") {
   vector<u32string_view> parts;
-  CHECK(RE32(",").split(U"a,b,c", parts) == 3);
-  CHECK(parts.size() == 3); CHECK(parts[0] == U"a"); CHECK(parts[1] == U"b"); CHECK(parts[2] == U"c");
+  CHECK(RE32(",").split(U"a,b,c", parts) == 3); CHECK(parts == vector{U"a"sv, U"b"sv, U"c"sv});
 
-  CHECK(RE32(",").split(U"a,b,c", parts, 1) == 2);
-  CHECK(parts.size() == 2); CHECK(parts[0] == U"a"); CHECK(parts[1] == U"b,c");
+  CHECK(RE32(",").split(U"a,b,c", parts, 1) == 2); CHECK(parts == vector{U"a"sv, U"b,c"sv});
 
-  CHECK(RE32(",").split(U",a,b,c,", parts) == 5);
-  CHECK(parts.size() == 5); CHECK(parts[0] == U""); CHECK(parts[1] == U"a");
-  CHECK(parts[2] == U"b"); CHECK(parts[3] == U"c"); CHECK(parts[4] == U"");
+  CHECK(RE32(",").split(U",a,b,c,", parts) == 5); CHECK(parts == vector{U""sv, U"a"sv, U"b"sv, U"c"sv, U""sv});
 
-  CHECK(RE32("\\d+").split(U"a42b1c1234", parts) == 4);
-  CHECK(parts.size() == 4); CHECK(parts[0] == U"a"); CHECK(parts[1] == U"b"); CHECK(parts[2] == U"c"); CHECK(parts[3] == U"");
+  CHECK(RE32("\\d+").split(U"a42b1c1234", parts) == 4); CHECK(parts == vector{U"a"sv, U"b"sv, U"c"sv, U""sv});
 
-  CHECK(RE32("(?<=[ab])\\d(?=b)").split(U"a1a2a3b4b6", parts) == 3);
-  CHECK(parts.size() == 3); CHECK(parts[0] == U"a1a2a"); CHECK(parts[1] == U"b"); CHECK(parts[2] == U"b6");
+  CHECK(RE32("(?<=[ab])\\d(?=b)").split(U"a1a2a3b4b6", parts) == 3); CHECK(parts == vector{U"a1a2a"sv, U"b"sv, U"b6"sv});
 }
 
 TEST_CASE("RE32::sub") {
