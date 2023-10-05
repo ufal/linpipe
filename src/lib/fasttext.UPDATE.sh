c4ba031295d165239e6e71d5cd95c8080f149293
Milan Straka <milan@strakovi.com>
2023-10-05 18:42:20 +0200

    Add fastText library.

---
+++ src/lib/fasttext.UPDATE.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# This file is part of C++-Builtem <http://github.com/ufal/cpp_builtem/>.
+#
+# Copyright 2014-2023 Institute of Formal and Applied Linguistics, Faculty
+# of Mathematics and Physics, Charles University in Prague, Czech Republic.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+set -e
+
+mkdir -p fasttext
+rm -f fasttext/*.h fasttext/*.cpp
+
+git clone https://github.com/facebookresearch/fastText fasttext_git
+git -C fasttext_git checkout v0.9.2
+
+for f in fasttext_git/src/*; do
+  case "$f" in
+    */main.cc) continue;;
+    *.cc) target=$(basename $f .cc).cpp;;
+    *) target=$(basename $f);;
+  esac
+  sed 's/namespace fasttext/namespace linpipe::fasttext/;
+       s/fasttext::/linpipe::fasttext::/' $f >fasttext/$target
+done
+
+rm -rf fasttext_git
