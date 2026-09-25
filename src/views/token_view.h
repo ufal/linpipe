8ca30d3ce0c5cf5e527507788b6ac408bd1b5836
Jana Strakova <strakova@ufal.mff.cuni.cz>
2026-09-25 11:31:31 +0200

    Remove TokenView iterator.
    Remove TokenView iterator to facilitate Python binding compatibility,
    have less code and not have to deal with iterators.

--- src/views/token_view.h
+++ src/views/token_view.h
@@ -16,24 +16,10 @@ namespace linpipe {
 
 class TokenView : public View {
   public:
-    struct const_iterator {
-      const TokenView* view;
-      size_t i;
-
-      const std::string& operator*() const { return view->text(i); }
-      const_iterator& operator++() { ++i; return *this; }
-      bool operator!=(const const_iterator& other) const { return i != other.i; }
-    };
-
     virtual ~TokenView() = default;
 
     virtual size_t size() const = 0;
     virtual const std::string& text(size_t i) const = 0;
-
-    const_iterator begin() const { return {this, 0}; }
-    const_iterator end() const { return {this, size()}; }
-
 };
-
 
 } // namespace linpipe
